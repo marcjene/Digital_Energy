c0ea1768c5b206f2eab84ede4d1b36fa388d16ff
Marc Jené <marc.jene@upc.edu>
2023-05-17 11:24:57 +0200

    Material curso Digital Energy

--- Codigos/SerialPort_to_PC/collecting_data_arduino_opencsv_student.py
+++ Codigos/SerialPort_to_PC/collecting_data_arduino_opencsv_student.py
@@ -12,14 +12,14 @@ import re
 import csv 
 
 # creating communications object using Serial 
-arduino = serial.Serial('COMX', BAUD_RATE) "Change X for number of COM channel, Change BAUD_RATE for baudrate"
+arduino = 
 print("Starting!")
 
 # try-except-finally loop for data acquisition
 try:
     while True:
-        now = dt.datetime.CLASSMETHOD() "Change CLASSMETHOD for classmethod of library datetime that gives the current local date and time"
-        # SEND MESSAGE
+        now = "Obtain date with library datetime that gives the current local date and time"
+        # SEND MESSAGE - Actuator (Relay, LED, etc.)
         """if (now.second%10<5):
             arduino.write('H'.encode())
             print ('LED ON')
@@ -28,8 +28,8 @@ try:
             print ('LED OFF')"""
             
         # READ DATA    
-        # Check if there is new info from the Arduino and read it
-        data_bytes = arduino.readline() 
+        # Check if there is new info from the Arduino and read it (.readline)
+        data_bytes = 
         # Decoding the message into UTF-8         
         data = data_bytes.decode("utf-8")
         
@@ -39,7 +39,7 @@ try:
             # strip data string into 2, 3 or as many different values for each reading   
             v1,v2 = re.findall(pattern=r"[-+]?\d*\.\d+|[-+]?\d+", string=data)  # to understand pattern: https://regex101.com/
             # store date and readings (change them to float!) into a list
-            row = [#date, #variable1, #variable2]
+            row = [] # date and variables
             # save reading row into the csv file. File needs to be open with "a" (append) mode.
             with open('FILE_NAME.csv', 'a', newline='') as csvFile:  "Change FILE_NAME to whatever you want (e.g.: 1103_test)"
                 writer = csv.writer(csvFile)
